202e30261b68a3a5d34e59f7b9396de89c28c99c
Jennifer Browning <browningjen@yahoo.com>
2018-11-05 09:14:30 -0500

    second commit

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 const express = require("express");
-const MongoClient = require('mongodb').MongoClient;
-const assert = require("assert");
+const mongoose = require('mongoose');
 const app = express();
-const publicPath = "public/";
+const url = 'mongodb://localhost:27017/rsvp'
+mongoose.connect(url);
+let db = mongoose.connection;
 
-const url = 'mongodb://localhost:27017';
-const dbName = 'rsvp';
-
-let mongoose = require('mongoose');
-let dbName = mongoose.connection;
+const port = 3000;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once("open", (err) => {
+    if (err) {
+        console.log(err);
+        throw err;
+    }
+    app.listen(port, () => {
+        console.log('server is running on port', port)
+    });
+})
 
 app.set("view engine", "pug");
+app.use(express.urlencoded());
 
-//create model
-let ReservationModel = mongoose.model('ReservationModel', rsvpSchema)
-
-//create an iteration of the mongoose
 let rsvpSchema = new mongoose.Schema({
     name: {type: String, required: true},
     email: {type: String, required: true},
@@ -23,15 +27,22 @@ let rsvpSchema = new mongoose.Schema({
     numberOfGuests:{type: Number}
 });
 
+//create model
+let ReservationModel = mongoose.model('ReservationModel', rsvpSchema)
+
+//create an iteration of the mongoose
+
+
 // get for the main page
 app.get("/", (request, response) => {
     response.render("index");
 });
 
-app.post('/', function (request, response, next) {
+app.post('/rsvpSubmitted', function (request, response, next) {
     let alias = request.body.name
     let contactEmail = request.body.email
     let yesOrNo = request.body.attending
+    console.log(request.body)
     let numberOfGuestsComing = request.body.number
     let newRSVP = new ReservationModel({
         name: alias,
@@ -39,13 +50,15 @@ app.post('/', function (request, response, next) {
         attending: yesOrNo,
         numberOfGuests: numberOfGuestsComing
     })
-    newRSVP.save(function(err, mewRSVP) {
+    newRSVP.save(function(err, newRSVP) {
         if(err) {
+        console.log(err)
         response.status(500)
         response.send();
         //to create an error page
-        }
-        response.render();
+        }else {
+        response.render("rsvpSubmitted");
+        };
         //always use render when you want to send back the final html page
     })
 
@@ -68,5 +81,5 @@ app.get('/guestList', function(request, response) {
 
 });
 
-app.listen(3000, ()=> {console.log("The Server has started on Port 3000")});
+
 
