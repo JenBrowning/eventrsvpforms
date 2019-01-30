0b56f80b9a7705097e41c741a54ed75dbcf12854
Jennifer Browning <browningjen@yahoo.com>
2019-01-30 12:39:01 -0500

    fixing errors

--- app.js
+++ app.js
@@ -1,85 +1,55 @@
 const express = require("express");
-const mongoose = require('mongoose');
-const app = express();
-const url = 'mongodb://localhost:27017/rsvp'
-mongoose.connect(url);
-let db = mongoose.connection;
+const mongoose = require("mongoose");
 
 const port = 3000;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once("open", (err) => {
-    if (err) {
-        console.log(err);
-        throw err;
-    }
-    app.listen(port, () => {
-        console.log('server is running on port', port)
-    });
-})
+const url = "mongodb://localhost:27017/rsvp";
+const app = express();
 
-app.set("view engine", "pug");
+app.use(express.static("public/"));
 app.use(express.urlencoded());
+app.set("view engine", "pug");
 
-let rsvpSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    email: {type: String, required: true},
-    attending: {type: Boolean},
-    numberOfGuests:{type: Number}
+mongoose.connect(url);
+
+const Schema = mongoose.Schema;
+const resSchema = new Schema({
+  name: String,
+  email: String,
+  attending: String,
+  guests: Number
 });
 
-//create model
-let ReservationModel = mongoose.model('ReservationModel', rsvpSchema)
+const Response = mongoose.model("Response", resSchema);
+const rsvp = mongoose.connection;
 
-//create an iteration of the mongoose
-
-
-// get for the main page
-app.get("/", (request, response) => {
-    response.render("index");
+rsvp.on("error", console.error.bind(console, "connection error"));
+rsvp.once("open", function() {
+  console.log("success");
 });
 
-app.post('/rsvpSubmitted', function (request, response, next) {
-    let alias = request.body.name
-    let contactEmail = request.body.email
-    let yesOrNo = request.body.attending
-    console.log(request.body)
-    let numberOfGuestsComing = request.body.number
-    let newRSVP = new ReservationModel({
-        name: alias,
-        email: contactEmail,
-        attending: yesOrNo,
-        numberOfGuests: numberOfGuestsComing
-    })
-    newRSVP.save(function(err, newRSVP) {
-        if(err) {
-        console.log(err)
-        response.status(500)
-        response.send();
-        //to create an error page
-        }else {
-        response.render("rsvpSubmitted");
-        };
-        //always use render when you want to send back the final html page
-    })
+app.listen(port, console.log("Listening on port " + port));
 
+app.get("/", function(req, res) {
+  res.render("getPug");
 });
 
-//get for the client to get the guest list
-app.get('/guestList', function(request, response) {
-    ReservationModel.find(function(err, reservations) {
-        if(err) {
-            response.status(500);
-            response.send();
-            return
-            //return here to get the function to stop.
-        }
-        response.render("guestList", {
-            attending: reservations.filter(reservation => reservationModel.attending === true),
-            notAttending: reservations.filter(reservation => reservationModel.attending === false)
-        });
-    });
-
+app.get("/guestlist", function(req, res) {
+  Response.find(function(err, userRes) {
+    if (err) return console.error(err);
+    console.log(userRes);
+    res.render("guestList", { userRes });
+  });
 });
 
-
-
+app.post("/rsvp", function(req, res) {
+  const userRes = new Response({
+    name: req.body.name,
+    email: req.body.email,
+    attending: req.body.attending,
+    guests: req.body.guests
+  });
+  userRes.save(function(err, userRes) {
+    if (err) return console.error(err);
+    res.render("postPug");
+  });
+});
